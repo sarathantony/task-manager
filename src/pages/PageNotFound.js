590d3c1da956f741327a7c0e29f40d9067e27ae3
Sarath Antony <sarathantony@yahoo.co.in>
2023-12-06 14:13:50 +0530

    feat: add a homepage link to the PageNotFound page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Link from '@mui/material/Link'
+
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
@@ -7,17 +9,24 @@ import Container from '@mui/material/Container'
 const PageNotFound = () => {
   return (
     <Container>
-      <Grid container sx={(theme) => ({ paddingTop: theme.spacing(6)})}>
+      <Grid container sx={(theme) => ({ paddingTop: theme.spacing(6) })}>
         <Grid item>
-          <Typography variant='subtitle1' gutterbottom>404 Error!</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant='h4' gutterBottom>Page not found</Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant='subtitle2' gutterBottom color='gray'>
-            Sorry! the page you are looking for could not be found or has been removed.
-          </Typography>
+          <Grid item>
+            <Typography variant='subtitle1' gutterbottom>404 Error!</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='h4' gutterBottom>Page not found</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='subtitle2' gutterBottom color='gray'>
+              Sorry! the page you are looking for could not be found or has been removed.
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='subtitle2' gutterBottom color='gray'>
+              Click <Link href='/'>here</Link> to go home
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
     </Container>
