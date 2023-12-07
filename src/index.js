efbdcf57b9f856238e0a178c5b3b26424840a223
Sarath Antony <sarathantony@yahoo.co.in>
2023-12-07 07:35:14 +0530

    fix: change BrowserRouter to HashRouter for deployment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import App from "./App.js"
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
